a626c4aba4b75e0ac48708cba776ddf33de4ee14
ghasalliyasser <gharsalliyasser01@outlook.com>
2020-08-18 14:22:38 +0200

    web page

---
+++ first page/photomania.js
@@ -0,0 +1,11 @@
+function myFunction() {
+  document.body.style.backgroundColor = "#f3f3f3";
+  document.body.style.backgroundImage = "url('imges.png')";
+}
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
